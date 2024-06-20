87018feecab2a0a97ecb6770880f3c5e34d2f994
Andre Felipe <andref.lopes@outlook.com>
2024-06-19 22:24:54 -0300

    adc comentários

--- heranca-em-python/agencia.py
+++ heranca-em-python/agencia.py
@@ -1,6 +1,10 @@
+# importando a Classe Mãe
 from banco import Banco
 
+# a Classe Mãe fica entre parênteses
 class Agencia(Banco):
     def __init__(self, nome_agencia, endereco_agencia, numero_agencia):
+        # método super() faz acessar tudo aquilo que está dentro da Classe Mãe
         super().__init__(nome_agencia, endereco_agencia)
-        self._numero = numero_agencia
+        # a linha abaixo representa um atributo próprio da Classe Filha
+        self._numero_agencia = numero_agencia
